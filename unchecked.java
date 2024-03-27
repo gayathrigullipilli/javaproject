773c956890f2e64d0c01691721afcad7fd1e8cf4
gayathrigullipilli <162116887+gayathrigullipilli@users.noreply.github.com>
2024-03-27 15:38:46 +0530

    Add files via upload

--- unchecked.java
+++ unchecked.java
@@ -1,11 +1,30 @@
 package exceptions;
 
+
 public class unchecked 
 {
 	public static void main(String [] args)
 	{
+		System.out.println("welcome");
+		try {
+			String x="abc";
+			int i=Integer.parseInt(x);
+			System.out.println(i);
+					
+			String s="abc";
+			System.out.println(s.charAt(3));
+			
 		int d=100/0;
 		System.out.println(d);
+		int a[]=new int[6];
+				System.out.println(a[7]);
+	}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		
+		}
+		System.out.println("hello");
 	}
 
 }
